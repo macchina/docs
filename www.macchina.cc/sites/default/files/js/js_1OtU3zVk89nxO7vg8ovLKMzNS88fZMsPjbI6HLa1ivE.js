65d2f26d30cf6a3d052dd4609b4960e6ad076f93
Adam Voss <vossad01@gmail.com>
2017-08-04 19:59:17 -0500

    Copy of the Macchina Guide fetched August 2nd, 2017

---
+++ www.macchina.cc/sites/default/files/js/js_1OtU3zVk89nxO7vg8ovLKMzNS88fZMsPjbI6HLa1ivE.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$("#block-multiblock-1").hide();$(".node-type-blog #node-comment-count").click(function(){$("#block-multiblock-1").slideToggle();$(this).toggleClass("closed")})});;
